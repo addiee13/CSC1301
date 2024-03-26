4b7184fcd60702bc2a773325848909843271aa48
Adam Dalwai <adamdalwai1@gmail.com>
2024-03-26 12:12:30 -0400

    Working version of names_n

--- names_n.py
+++ names_n.py
@@ -1,7 +1,7 @@
-# names = ["riyan", "Rohaan", "Ayuhsman", "Adam", "Nousheen", "Sneh", "Harini", "Krisha"]
-# names_n = []
-# for name in names:
-#     if name[-1] == 'n':
-#         names_n.append(name)
-# for name in names_n:
-#     print(name.title())
+names = ["riyan", "Rohaan", "Ayuhsman", "Adam", "Nousheen", "Sneh", "Harini", "Krisha"]
+names_n = []
+for name in names:
+    if name[-1] == 'n':
+        names_n.append(name)
+for name in names_n:
+    print(name.title())
