8019c3ebee7e593d1b1ea541081169d748a4a0bc
Adam Dalwai <adamdalwai1@gmail.com>
2024-04-02 02:26:19 -0400

    changed test.py

--- file_processing/test.py
+++ file_processing/test.py
@@ -1,2 +1,7 @@
-with open("jfaefn.txt", "w"):
-    print("he")
+with open("name.txt", "r") as infile:
+    data = infile.read()
+
+lines = data.splitlines()
+
+for line in lines:
+    print(line.strip(" .?"))
