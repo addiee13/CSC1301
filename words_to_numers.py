2197961d04511c6f2e069cdd5f09642b72a0a66f
Adam Dalwai <adamdalwai1@gmail.com>
2024-04-03 21:02:32 -0400

    Added words_to_numers

---
+++ words_to_numers.py
@@ -0,0 +1,10 @@
+def word_to_number():
+    words_to_numbers = {
+        "one" : 1,
+        "two" : 2,
+        "three" : 3,
+        "four" : 4,
+        "five" : 5
+    }
+    return words_to_numbers
+print(word_to_number())
