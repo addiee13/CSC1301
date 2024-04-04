df067124fbf6cfc3d56f13f663e5b2f548816781
Adam Dalwai <adamdalwai1@gmail.com>
2024-04-04 13:32:09 -0400

    Changed dir name

---
+++ chp_8/clubs.py
@@ -0,0 +1,6 @@
+students = ["Adam", "Riyan", "Mahean", "Parsa", "Ayuuhman"]
+cs_club = ["Adam", "Riyan"]
+math_club = ["Mahean"]
+for student in students:
+    if student not in cs_club and student not in math_club:
+        print(student)
